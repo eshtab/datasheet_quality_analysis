bc244b141eb46bc8b26438c3d1abd6c440531bdf
Eshta Bhardwaj <eshta.bhardwaj@gmail.com>
2023-04-18 19:42:22 -0400

    edits to scripts

---
+++ scripts/01-data_cleaning.R
@@ -0,0 +1,71 @@
+#### Preamble ####
+# Purpose: Cleaning and formatting datasheets
+# Author: Eshta Bhardwaj
+# Date: April 21 2023
+# Contact: eshta.bhardwaj@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: Must already have pdfs of datasheets downloaded. Pdf must be in single column format.
+
+#### Workspace set-up ####
+library(tidyverse)
+library(pdftools)
+
+#### reading in blank datasheet to establish baselines ####
+
+# 2021
+
+datasheet_questions_2021 <- pdftools::pdf_text("inputs/data/2021_Datasheet_questionsonly.pdf")
+datasheet_questions_2021
+
+datasheet_questions_2021tbl <- tibble(
+  raw_text = datasheet_questions_2021,
+  page_number = c(1:6)
+)
+
+datasheet_questions_2021tbl <-
+  separate_rows(datasheet_questions_2021tbl, raw_text, sep = "\\n", convert = FALSE)
+
+# 2018
+
+datasheet_questions_2018 <- pdftools::pdf_text("inputs/data/2018_Datasheet_questionsonly.pdf")
+datasheet_questions_2018
+
+datasheet_questions_2018tbl <- tibble(
+  raw_text = datasheet_questions_2018,
+  page_number = c(1:3)
+)
+
+datasheet_questions_2018tbl <-
+  separate_rows(datasheet_questions_2018tbl, raw_text, sep = "\\n", convert = FALSE)
+
+#### reading in datasheets ####
+
+## datasheet1: A sandbox for prediction and integration of DNA, RNA, and proteins in single cells
+
+datasheet1 <- pdftools::pdf_text("inputs/data/raw/datasheet1.pdf")
+datasheet1
+
+datasheet1_tbl <- tibble(
+  raw_text = datasheet1,
+  page_number = c(1:7)
+)
+
+datasheet1_tbl <-
+  separate_rows(datasheet1_tbl, raw_text, sep = "\\n", convert = FALSE)
+
+# remove rows that are blank in first column
+# source: https://stackoverflow.com/questions/9126840/delete-rows-with-blank-values-in-one-particular-column
+datasheet1_tbl <- datasheet1_tbl[-which(datasheet1_tbl$raw_text == "")]
+
+# create column that identifies section header, question rows, and answer rows
+# this will enable counting for completion and word count 
+
+
+# source: https://www.tutorialspoint.com/how-to-check-whether-a-string-is-in-lowercase-or-uppercase-in-r#:~:text=We%20can%20use%20str_detect%20function,is%20in%20lowercase%20or%20uppercase.
+if (str_detect(x2,"[[:upper:]]") == TRUE){
+  datasheet1_tbl$text_type = 'header'
+} else{
+    
+  
+  }
+     
